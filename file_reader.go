0b63a43610ed54c64b49a6df232b17e4fb17269e
LaTotty <latotty91@gmail.com>
2016-06-09 22:51:09 +0200

    support for file input restructure code a bit

---
+++ file_reader.go
@@ -0,0 +1,42 @@
+/*
+	Forked from https://github.com/samuell/glow
+*/
+
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// FileReader is a file reader stream
+type FileReader struct {
+	InFilePath chan string
+	Out        chan []byte
+}
+
+func (fileReader *FileReader) OutChan() chan []byte {
+	fileReader.Out = make(chan []byte, 16)
+	return fileReader.Out
+}
+
+func (fileReader *FileReader) Init() {
+	go func() {
+		file, err := os.Open(<-fileReader.InFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scan := bufio.NewScanner(file)
+		for scan.Scan() {
+			fileReader.Out <- append([]byte(nil), scan.Bytes()...)
+		}
+		if scan.Err() != nil {
+			log.Fatal(scan.Err())
+		}
+
+		close(fileReader.Out)
+	}()
+}
--- lolcat.go
+++ lolcat.go
@@ -1,24 +1,75 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
+
+type inputTypeEnum int
+
+const (
+	inputTypeStdin inputTypeEnum = iota
+	inputTypeFile
+)
+
+type configStruct struct {
+	wrapWidth int
+	inputType inputTypeEnum
+	filePath  string
+}
 
 func main() {
-	var wrapWidth int
-	flag.IntVar(&wrapWidth, "w", 0, "wrap width.  defaults to 0 / off.")
+	config := configStruct{}
+
+	parseArgs(&config)
+
+	start(&config)
+}
+
+func parseArgs(config *configStruct) {
+	flag.IntVar(&config.wrapWidth, "w", 0, "wrap width.  defaults to 0 / off.")
 	flag.Parse()
 
+	args := flag.Args()
+
+	switch len(args) {
+	case 0:
+		config.inputType = inputTypeStdin
+	case 1:
+		config.inputType = inputTypeFile
+		config.filePath = args[0]
+	default:
+		log.Fatal("Invalid arguments length")
+	}
+}
+
+func start(config *configStruct) {
 	// Create components, connecting the channels
-	stdInReader := new(StdInReader)
 	printer := new(Printer)
 	wrapper := new(Wrapper)
 
+	var input interface {
+		Init()
+		OutChan() chan []byte
+	}
+
+	switch config.inputType {
+	case inputTypeFile:
+		fileReader := new(FileReader)
+		fileReader.InFilePath = make(chan string, 1) // buffered so the insert wont block
+		fileReader.InFilePath <- config.filePath
+		input = fileReader
+	default:
+		input = new(StdInReader)
+	}
+
 	// Connect them
-	wrapper.In = stdInReader.OutChan()
+	wrapper.In = input.OutChan()
 	printer.In = wrapper.OutChan()
 
 	// Start them
-	stdInReader.Init()
-	wrapper.Init(wrapWidth)
+	input.Init()
+	wrapper.Init(config.wrapWidth)
 	printer.Init()
 
 	// Wait until the end
